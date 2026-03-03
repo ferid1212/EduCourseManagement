9088cb4820096b3273bbf30cbae15382056cb596
ferid1212 <feridmamedov2008@gmail.com>
2026-03-03 08:23:13 -0800

    First commit

---
+++ src/main/java/com/example/educoursemanagementsystem/EduCourseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.educoursemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduCourseManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduCourseManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/educoursemanagementsystem/EduCourseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educoursemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduCourseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
