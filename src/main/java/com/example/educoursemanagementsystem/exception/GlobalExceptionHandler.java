eafa3b00f7a8703229574aabb62c0295a88b169e
ferid1212 <feridmamedov2008@gmail.com>
2026-03-25 08:20:41 -0700

    GlobalExceptionHandler is created

---
+++ src/main/java/com/example/educoursemanagementsystem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.example.educoursemanagementsystem.exception;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<?> handleAlreadyExistsException(AlreadyExistsException ex){
+        log.warn("AlreadyExistsException happened: {}",ex.getMessage());
+        return ResponseEntity.status(HttpStatus.ALREADY_REPORTED).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex){
+        log.warn("ResourceNotFoundException happened: {}",ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+
+
+}
