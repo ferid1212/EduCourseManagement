cd2a531a9f80a5dde4261cd48ce7983e580d97fb
ferid1212 <feridmamedov2008@gmail.com>
2026-04-04 11:43:02 -0700

    Login validation modified

--- src/main/java/com/example/educoursemanagementsystem/model/dto/request/AuthRequest.java
+++ src/main/java/com/example/educoursemanagementsystem/model/dto/request/AuthRequest.java
@@ -1,9 +1,11 @@
 package com.example.educoursemanagementsystem.model.dto.request;
 
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 @Data
 public class AuthRequest {
+    @Email
     private String email;
     private String password;
 }
--- src/main/java/com/example/educoursemanagementsystem/model/dto/request/RegisterRequest.java
+++ src/main/java/com/example/educoursemanagementsystem/model/dto/request/RegisterRequest.java
@@ -1,14 +1,23 @@
 package com.example.educoursemanagementsystem.model.dto.request;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+    @NotBlank
     private String firstName;
+    @NotBlank
     private String lastName;
+    @Email
     private String email;
+    @Pattern(regexp = "\\d{6,}", message = "Minimum 6 rəqəm olmalıdır")
     private String password;
     private Long courseId;
+    @NotNull(message = "Age is required")
+    @Min(value = 18, message = "Age must be at least 18")
+    @Max(value = 100, message = "Age cannot exceed 100")
     private Integer age;
+    @Pattern(regexp = "^994(50|51|55|99|70|77|10)\\d{7}$",message = "Please,Enter correct format for phone")
     private String phone;
 }
